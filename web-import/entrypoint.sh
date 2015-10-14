55273f031858c374ceb030628bba14cdce43532b
Neel Radhakrishnan <neelradhakrishnan@gmail.com>
2015-10-14 14:42:40 +0100

    import uses tar file

--- web-import/entrypoint.sh
+++ web-import/entrypoint.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
+# untar data to root. tar structure reflects docker volume paths
+tar -C / -xvf data.tar.gz
+
+# make web files owned by www-data
+chown -R www-data /var/www
+chgrp -R www-data /var/www
+
+# wait for mariadb container to be ready for import
 while ! mysqladmin ping -h web-mariadb --silent; do
     sleep 1s
 done
 
-mysql -h web-mariadb < /tmp/db.sql
-
+# import database dump and flush priviliges
+mysql -h web-mariadb < /tmp/db/db.sql
 mysql -h web-mariadb -e 'FLUSH PRIVILEGES;'
 
-rm /tmp/db.sql
+# remove import data to save space
+rm -r /tmp/db
+rm data.tar.gz
