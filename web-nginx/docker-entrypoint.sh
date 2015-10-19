3d68699ba417858d8f4d41192520c2bbec9fafa8
Neel Radhakrishnan <neelradhakrishnan@gmail.com>
2015-10-19 02:24:55 +0100

    fix wordpress permissions

--- web-nginx/docker-entrypoint.sh
+++ web-nginx/docker-entrypoint.sh
@@ -14,12 +14,12 @@ rm -f /var/www/files-imported
 # make web files owned by nginx
 chown -R nginx:nginx /var/www
 
-# set permissons
+# set permissons (from macbleser github script)
 find /var/www -type d -exec chmod 755 {} \;
 find /var/www -type f -exec chmod 644 {} \;
 find /var/www -name "wp-config.php" -exec chmod 660 {} \;
-find /var/www -name "wp-content" -exec chmod -R 775 {} \;
-find /var/www -name "wp-content" -exec chmod 664 {} \;
+find /var/www -name "wp-content" -exec chmod 775 {} \;
+find /var/www -name "wp-content" -exec chmod -R 664 {} \;
 
 # start nginx
 nginx -g "daemon off;"
