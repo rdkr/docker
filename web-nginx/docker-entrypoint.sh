2340a27b18ad5f4070e3635478cc5a457ac3f717
Neel Radhakrishnan <neelradhakrishnan@gmail.com>
2015-10-19 03:12:43 +0100

    add fs method to wp configs

--- web-nginx/docker-entrypoint.sh
+++ web-nginx/docker-entrypoint.sh
@@ -18,8 +18,12 @@ chown -R nginx:nginx /var/www
 find /var/www -type d -exec chmod 755 {} \;
 find /var/www -type f -exec chmod 644 {} \;
 find /var/www -name "wp-config.php" -exec chmod 660 {} \;
+find /var/www -name "wp-config.php" -exec echo "define ('FS_METHOD','direct')" >> {} \;
 for i in $(find /var/www -name "wp-content" -type d); do find $i -type d -exec chmod 775 {} \;; done
 for i in $(find /var/www -name "wp-content" -type d); do find $i -type f -exec chmod 664 {} \;; done
+
+# add fs method to wp configs (not sure why this is necessary!?)
+for i in $(find /var/www -name "wp-config.php"); do echo "define ('FS_METHOD','direct');" >> $i; done;
 
 # start nginx
 nginx -g "daemon off;"
