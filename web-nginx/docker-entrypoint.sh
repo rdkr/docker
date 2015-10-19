036d5633794d6003af692e4e30955d8677264fee
Neel Radhakrishnan <neelradhakrishnan@gmail.com>
2015-10-19 02:18:59 +0100

    wordpress permissions

--- web-nginx/docker-entrypoint.sh
+++ web-nginx/docker-entrypoint.sh
@@ -14,4 +14,12 @@ rm -f /var/www/files-imported
 # make web files owned by nginx
 chown -R nginx:nginx /var/www
 
+# set permissons
+find /var/www -type d -exec chmod 755 {} \;
+find /var/www -type f -exec chmod 644 {} \;
+find /var/www -name "wp-config.php" -exec chmod 660 {} \;
+find /var/www -name "wp-content" -exec chmod -R 775 {} \;
+find /var/www -name "wp-content" -exec chmod 664 {} \;
+
+# start nginx
 nginx -g "daemon off;"
