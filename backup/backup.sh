87048728e2e59c11f0b20852a257b4559634cd04
Neel Radhakrishnan <neelradhakrishnan@gmail.com>
2016-01-19 23:39:19 +0000

    backup cron working, using phusion baseimage

--- backup/backup.sh
+++ backup/backup.sh
@@ -2,23 +2,13 @@
 
 set -e
 
-if [ "$1" != 'incremental' ] && [ "$1" != 'full' ]; then
-
-	exit
-
-fi
+if [ "$1" != 'incremental' ] && [ "$1" != 'full' ]; then exit; fi
  
-echo mysqldump
-
 mysqldump -h web-mariadb -u root --single-transaction --all-databases > /tmp/db.sql
-
-echo duplicity
 
 duplicity $1 --no-encryption --allow-source-mismatch /var/www file:///mnt/www
 duplicity $1 --no-encryption --allow-source-mismatch /tmp/db.sql file:///mnt/db
-duplicity $1 --no-encryption --allow-source-mismatch /znc-data file:///mnt/znc
-duplicity $1 --no-encryption --allow-source-mismatch /teamspeak3 file:///mnt/ts3
+duplicity $1 --no-encryption --allow-source-mismatch /znc-data file:///mnt/znc-data
+duplicity $1 --no-encryption --allow-source-mismatch /teamspeak3 file:///mnt/teamspeak3
 
 rm /tmp/db.sql
-
-echo done
