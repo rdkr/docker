2b132d7c9eba1cfb041fa3c117533d8e86decae0
Neel Radhakrishnan <neelradhakrishnan@gmail.com>
2016-05-25 01:07:22 +0100

    updated to use certbot

--- web-letsencrypt/docker-entrypoint.sh
+++ web-letsencrypt/docker-entrypoint.sh
@@ -2,13 +2,4 @@
 
 set -e
 
-if [ "$1" = 'standalone' ]; then
-    
-    mkdir -p /etc/letsencrypt/webroot
-
-    letsencrypt certonly -c /cli.ini -a standalone
-
-elif [ "$1" = 'webroot' ]; then
-
-    letsencrypt certonly -c /cli.ini -a webroot
-fi
+certbot certonly --text --non-interactive --agree-tos --rsa-key-size 4096 --webroot --webroot-path  /etc/letsencrypt/webroot --email neelradhakrishnan@gmail.com --domains rdkr.uk,neel.rdkr.uk,www.neel.rdkr.uk,www.rdkr.uk,www.brumlions.co.uk,brumlions.co.uk,www.rdkr.ca,rdkr.ca,www.saveoursudbrooke.uk,saveoursudbrooke.uk,forum.saveoursudbrooke.uk,www.sudbrookepreschool.org.uk,sudbrookepreschool.org.uk
