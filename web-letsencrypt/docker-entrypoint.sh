fe93289fdb7a1b553b838504cef091d56e8fc52d
Neel Radhakrishnan <neelradhakrishnan@gmail.com>
2016-06-02 03:02:53 +0100

    add scgf.rdkr.uk

--- web-letsencrypt/docker-entrypoint.sh
+++ web-letsencrypt/docker-entrypoint.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-certbot certonly --text --non-interactive --agree-tos --expand --rsa-key-size 4096 --webroot --webroot-path  /etc/letsencrypt/webroot --email neelradhakrishnan@gmail.com --domains rdkr.uk,neel.rdkr.uk,www.neel.rdkr.uk,www.rdkr.uk,www.brumlions.co.uk,brumlions.co.uk,www.rdkr.ca,rdkr.ca,www.saveoursudbrooke.uk,saveoursudbrooke.uk,forum.saveoursudbrooke.uk,www.sudbrookepreschool.org.uk,sudbrookepreschool.org.uk,brexit.review,www.brexit.review
+certbot certonly --text --non-interactive --agree-tos --expand --rsa-key-size 4096 --webroot --webroot-path  /etc/letsencrypt/webroot --email neelradhakrishnan@gmail.com --domains rdkr.uk,neel.rdkr.uk,www.neel.rdkr.uk,www.rdkr.uk,scgf.rdkr.uk,www.brumlions.co.uk,brumlions.co.uk,www.rdkr.ca,rdkr.ca,www.saveoursudbrooke.uk,saveoursudbrooke.uk,forum.saveoursudbrooke.uk,www.sudbrookepreschool.org.uk,sudbrookepreschool.org.uk,brexit.review,www.brexit.review
