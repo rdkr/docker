7d3856121233169ec7bba926257c8fdd6dc09c42
Neel Radhakrishnan <neel@rdkr.uk>
2016-08-01 17:43:07 +0100

    added encryption key, loop through backups

--- data-backup/backup.sh
+++ data-backup/backup.sh
@@ -4,8 +4,16 @@ set -e
  
 mysqldump -h web-mariadb -u root --single-transaction --all-databases > /var/www/db.sql
 
-duplicity --no-encryption --full-if-older-than 1M --s3-use-new-style /var/www s3://s3.amazonaws.com/rdkr-web
-duplicity --no-encryption --full-if-older-than 1M --s3-use-new-style /etc/letsencrypt s3://s3.amazonaws.com/rdkr-le
-duplicity --no-encryption --full-if-older-than 1M --s3-use-new-style /teamspeak3 s3://s3.amazonaws.com/rdkr-ts3
+for i in \
+	"/var/www s3://s3.amazonaws.com/rdkr-web" \
+	"/etc/letsencrypt s3://s3.amazonaws.com/rdkr-le" \
+	"/teamspeak3 s3://s3.amazonaws.com/rdkr-ts3" ; do
+		
+  duplicity --encrypt-key="F7887127" \
+    		--sign-key="F7887127" \
+    		--full-if-older-than 1M \
+    		--s3-use-multiprocessing \
+    		--s3-use-new-style $i
+done 
 
 rm /var/www/db.sql
